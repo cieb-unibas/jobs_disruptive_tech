b49b02daa4324b9e59739740901cc9565da6d1e2
MatthNig <matthias.niggli@unibas.ch>
2022-09-23 17:48:01 +0200

    map legend changed

--- R/create_map.R
+++ R/create_map.R
@@ -7,31 +7,30 @@ package_setup(packages = c("tidyverse", "sf", "viridis"))
 
 #### load data ####
 map_df <- read.csv("data/map_df.csv")
-comp_df <- read.csv(
-  file = "/scicore/home/weder/GROUP/Innovation/05_job_adds_data/augmentation_data/ch_total_employed_people_02_2022.csv",
-  sep = ";")
+# comp_df <- read.csv(
+#   file = "/scicore/home/weder/GROUP/Innovation/05_job_adds_data/augmentation_data/ch_total_employed_people_02_2022.csv",
+#   sep = ";")
 GEO_PATH <- "/scicore/home/weder/GROUP/ch_geo/2019_THK_PRO/PRO/03_ANAL/GesamtflДche_gf/K4_greg20001205_gf/"
 geo <- st_read(paste0(GEO_PATH, "K4greg20001205gf_ch2007Poly.shp"))
 map_df <- geo %>%
   mutate(nuts_2 = paste0("CH0", seq(7))) %>%
   select(geometry, nuts_2) %>%
   left_join(map_df, by = "nuts_2") %>%
-  na.omit() %>%
-  head()
+  na.omit()
 
 #### Plot Specialization
 text_labels <- map_df %>% select(regio_bloom_share ) %>%
   mutate(regio_bloom_share  = scales::percent(regio_bloom_share , accuracy = 0.1)) %>%
   cbind(st_coordinates(st_centroid(map_df$geometry)))
-
 ggplot(data = map_df)+
   geom_sf(aes(fill = regio_bloom_share ), alpha = 0.9) +
   geom_text(data = text_labels, aes(x = X, y = Y, label = regio_bloom_share ), 
             size = 3, vjust = 2, hjust = -0.3) +
-  scale_fill_viridis(name="Share of Job Postings\n with a connection to\n disruptive technologies", option = "plasma", 
+  scale_fill_viridis(name=" Share of Job Postings\n with a connection to\n disruptive technologies", option = "plasma", 
                      begin = 0.3, end = 0.85,
                      labels = scales::percent) +
-  theme_void()
+  theme_void() +
+  theme(legend.position = "bottom")
 ggsave("img/nuts_postings_specialization.png")
 
 #### SOURCES AND CREDITS ####
