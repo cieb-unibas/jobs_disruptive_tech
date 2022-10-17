51d8bb7b59fd190d0d8a095f07fde022628c343e
MatthNig <matthias.niggli@unibas.ch>
2022-10-17 17:55:05 +0200

    company query with check that no duplicates are possible

--- R/jpod_queries.R
+++ R/jpod_queries.R
@@ -24,7 +24,7 @@ JPOD_QUERIES["bloom_nuts"] <- "
     "
 
 JPOD_QUERIES["bloom_companies"] <- "
-    SELECT pc.company_name, bt.bloom_field, COUNT(DISTINCT(bt.uniq_id)) as bloom_postings
+    SELECT pc.company_name, bt.bloom_field, COUNT(DISTINCT(jp.job_description)) as bloom_postings
     FROM (
         SELECT uniq_id, bloom_field
         FROM bloom_tech
@@ -33,6 +33,10 @@ JPOD_QUERIES["bloom_companies"] <- "
         SELECT uniq_id, company_name
         FROM position_characteristics
         ) pc on pc.uniq_id = bt.uniq_id
+    LEFT JOIN (
+        SELECT uniq_id, job_description
+        FROM job_postings
+        ) jp on jp.uniq_id = bt.uniq_id
     GROUP BY pc.company_name, bt.bloom_field
     ORDER BY bloom_postings DESC
     "
